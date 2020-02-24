4e02afd612fccbe2e16d37eef7fee16833ff78c7
Chandan Kumar <cksharma122@gmail.com>
2020-02-24 19:52:56 +0530

    Add nav styles

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -19,7 +19,7 @@ const Home = () => {
   return (
     <div className="container">
       <div className="navbar">
-        <Navbar headings={headings} onClick={setContentIndex} />
+        <Navbar headings={headings} onClick={setContentIndex} selectedNav={contentIndex}/>
       </div>
       <div className="content">
         <Content content={headings[contentIndex].content} />
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,14 @@
 import React from "react";
 
-const Navbar = ({ headings, onClick }) => {
+const Navbar = ({ headings, onClick, selectedNav }) => {
   return (
     <div className="headings">
       {headings.map((h, i) => (
-        <div key={h} onClick={onClick.bind(null, i)} className="heading">
+        <div
+        key={h}
+        onClick={onClick.bind(null, i)}
+        className={`${i === selectedNav ? "selected-nav heading" : "heading"}`}
+        >
           {h.title}
         </div>
       ))}
