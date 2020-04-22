ab1bc39916574e99c162d4870e59a0ad76c86c2a
Chandan Kumar <cksharma122@gmail.com>
2020-04-22 12:03:40 +0530

    Add twitter url

--- src/resources/profileLinks.js
+++ src/resources/profileLinks.js
@@ -1,18 +1,21 @@
 const urls = {
   profile_pic:
-    "https://i.ibb.co/4Mf8ysK/Screenshot-2020-02-24-at-11-34-46-PM.png",
-  facebook_logo: "https://www.facebook.com/images/fb_icon_325x325.png",
+    'https://i.ibb.co/4Mf8ysK/Screenshot-2020-02-24-at-11-34-46-PM.png',
+  facebook_logo: 'https://www.facebook.com/images/fb_icon_325x325.png',
   instagram_logo:
-    "https://blog.opennemas.com/media/blog/images/2017/11/21/2017112117323379600.png",
+    'https://blog.opennemas.com/media/blog/images/2017/11/21/2017112117323379600.png',
   github_logo:
-    "https://github.githubassets.com/images/modules/logos_page/Octocat.png",
+    'https://github.githubassets.com/images/modules/logos_page/Octocat.png',
   linkedin_logo:
-    "https://icons-for-free.com/iconfiles/png/512/linked+linkedin+logo+social+icon-1320191784782940875.png",
-  medium_logo:"https://cdn.onlinewebfonts.com/svg/img_256332.png",
-  facebook_url: "https://www.facebook.com/Cksharma11",
-  instagram_url: "https://www.instagram.com/_.struggler/",
-  github_url: "https://github.com/cksharma11",
-  linkedin_url: "https://www.linkedin.com/in/chandan-kumar-114485119/"
+    'https://icons-for-free.com/iconfiles/png/512/linked+linkedin+logo+social+icon-1320191784782940875.png',
+  medium_logo: 'https://cdn.onlinewebfonts.com/svg/img_256332.png',
+  twitter_logo:
+    'https://www.aps.edu/sapr/images/pnglot.comtwitterbirdlogopng139932.png/image',
+  facebook_url: 'https://www.facebook.com/Cksharma11',
+  instagram_url: 'https://www.instagram.com/_.struggler/',
+  github_url: 'https://github.com/cksharma11',
+  linkedin_url: 'https://www.linkedin.com/in/chandan-kumar-114485119/',
+  twitter_url: 'https://twitter.com/iamCKsharma',
 };
 
 export default urls;
