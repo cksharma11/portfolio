a0bf4db69fbba21d3849b8263fe772b723201bd0
Chandan Kumar <cksharma@thoughtworks.com>
2022-12-08 18:52:58 +0530

    Update portfolio

--- src/components/HeaderTitle/HeaderTitle.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const HeaderTitle = ({ title }) => {
-  return <h1 className='header-title font-blue'>{title}</h1>;
-};
-
-export default HeaderTitle;
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import * as serviceWorker from './serviceWorker';
-import Home from './components/Home/Home';
-
-ReactDOM.render(<Home />, document.getElementById('root'));
-
-serviceWorker.unregister();
